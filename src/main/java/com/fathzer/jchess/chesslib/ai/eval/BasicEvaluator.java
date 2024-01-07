24a24a4ae453ceabde65be5b5f3da27297c450e0
Fathzer <jm-dev@astesana.net>
2024-01-07 20:13:30 +0100

    Simplifies basic evaluator implementation

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/BasicEvaluator.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/BasicEvaluator.java
@@ -1,9 +1,6 @@
 package com.fathzer.jchess.chesslib.ai.eval;
 
-import static com.fathzer.games.Color.*;
-
 import com.fathzer.chess.utils.evaluators.NaiveEvaluator;
-import com.fathzer.games.Color;
 import com.fathzer.jchess.chesslib.ChessLibAdapter;
 import com.fathzer.jchess.chesslib.ChessLibMoveGenerator;
 import com.github.bhlangonijr.chesslib.move.Move;
@@ -15,15 +12,6 @@ public class BasicEvaluator extends NaiveEvaluator<Move, ChessLibMoveGenerator> 
 
 	private BasicEvaluator(int score) {
 		super(score);
-	}
-	
-	@Override
-	public void setViewPoint(Color viewPoint) {
-		if (viewPoint==null) {
-			this.viewPoint = 0;
-		} else {
-			this.viewPoint = viewPoint==WHITE ? 1 : -1;
-		}
 	}
 
 	@Override
