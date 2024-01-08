0d76525d43508336e1317740c95696eb161777ce
Fathzer <jm-dev@astesana.net>
2024-01-08 18:26:23 +0100

    Adapts to chess-utils changes

--- src/main/java/com/fathzer/jchess/chesslib/ChessLibAdapter.java
+++ src/main/java/com/fathzer/jchess/chesslib/ChessLibAdapter.java
@@ -1,22 +1,21 @@
 package com.fathzer.jchess.chesslib;
 
 import static com.fathzer.chess.utils.Pieces.*;
-import static com.fathzer.jchess.chesslib.BoardExplorerBuilder.*;
-
-import java.util.stream.Stream;
 
 import com.fathzer.chess.utils.adapters.MoveAdapter;
-import com.fathzer.chess.utils.adapters.PieceStreamer;
-import com.github.bhlangonijr.chesslib.Board;
+import com.fathzer.chess.utils.adapters.BoardExplorer;
+import com.fathzer.chess.utils.adapters.BoardExplorerBuilder;
 import com.github.bhlangonijr.chesslib.Piece;
 import com.github.bhlangonijr.chesslib.PieceType;
+import com.github.bhlangonijr.chesslib.Side;
 import com.github.bhlangonijr.chesslib.Square;
 import com.github.bhlangonijr.chesslib.move.Move;
 
-public interface ChessLibAdapter extends PieceStreamer<ChessLibMoveGenerator>, MoveAdapter<Move, ChessLibMoveGenerator> {
+public interface ChessLibAdapter extends BoardExplorerBuilder<ChessLibMoveGenerator>, MoveAdapter<Move, ChessLibMoveGenerator> {
+
 	@Override
-	default Stream<Integer> getPieces(ChessLibMoveGenerator board) {
-		return BoardExplorerBuilder.getPieces(board.getBoard());
+	default BoardExplorer getExplorer(ChessLibMoveGenerator board) {
+		return new ChessLibBoardExplorer(board.getBoard());
 	}
 
 	@Override
@@ -25,14 +24,13 @@ public interface ChessLibAdapter extends PieceStreamer<ChessLibMoveGenerator>, M
 	}
 	
 	@Override
-	default int getCapturedType(ChessLibMoveGenerator mv, Move move) {
-		final Board board = mv.getBoard();
-		final Piece moving = board.getPiece(move.getFrom());
-		if (Square.NONE!=board.getEnPassantTarget() && PieceType.PAWN==moving.getPieceType() &&
+	default int getCapturedType(ChessLibMoveGenerator board, Move move) {
+		final Piece moving = board.getBoard().getPiece(move.getFrom());
+		if (Square.NONE!=board.getBoard().getEnPassantTarget() && PieceType.PAWN==moving.getPieceType() &&
         move.getTo().getFile()!=move.getFrom().getFile()) {
 			return PAWN; // A pawn is captured
 		} else {
-			final Piece piece = board.getPiece(move.getTo());
+			final Piece piece = board.getBoard().getPiece(move.getTo());
 			// Be aware of castling in chess 960 where we can consider the king captures its own rook!
 			return piece.getPieceSide()!=moving.getPieceSide() ? fromPieceType(piece.getPieceType()) : 0;
 		}
@@ -41,5 +39,14 @@ public interface ChessLibAdapter extends PieceStreamer<ChessLibMoveGenerator>, M
 	@Override
 	default int getPromotionType(ChessLibMoveGenerator board, Move move) {
 		return fromPieceType(move.getPromotion().getPieceType());
+	}
+	
+	private static int fromPieceType(PieceType type) {
+		return type==null ? 0 : type.ordinal()+1;
+	}
+	
+	private static int toPiece(Piece piece) {
+		final int p = fromPieceType(piece.getPieceType());
+		return piece.getPieceSide()==Side.WHITE ? p : -p;
 	}
 }
---
+++ src/main/java/com/fathzer/jchess/chesslib/ChessLibBoardExplorer.java
@@ -0,0 +1,64 @@
+package com.fathzer.jchess.chesslib;
+
+import static com.github.bhlangonijr.chesslib.Square.*;
+
+import com.fathzer.chess.utils.adapters.BoardExplorer;
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.PieceType;
+import com.github.bhlangonijr.chesslib.Side;
+import com.github.bhlangonijr.chesslib.Square;
+
+class ChessLibBoardExplorer implements BoardExplorer {
+	private static final Square[] SQUARES;
+	
+	static {
+		SQUARES = new Square[] {
+			A8, B8, C8, D8, E8, F8, G8, H8,
+			A7, B7, C7, D7, E7, F7, G7, H7,
+			A6, B6, C6, D6, E6, F6, G6, H6,
+			A5, B5, C5, D5, E5, F5, G5, H5,
+			A4, B4, C4, D4, E4, F4, G4, H4,
+			A3, B3, C3, D3, E3, F3, G3, H3,
+			A2, B2, C2, D2, E2, F2, G2, H2,
+			A1, B1, C1, D1, E1, F1, G1, H1,
+		};
+	}
+	
+	private final Board board;
+	private int index;
+	private int piece;
+	
+	ChessLibBoardExplorer(Board board) {
+		this.board = board;
+		this.index = -1;
+		next();
+	}
+	
+	@Override
+	public boolean next() {
+		index++;
+		if (index>=SQUARES.length) {
+			return false;
+		}
+		piece = toPiece(board.getPiece(SQUARES[index]));
+		return piece != 0 || next();
+	}
+
+	static int fromPieceType(PieceType type) {
+		return type==null ? 0 : type.ordinal()+1;
+	}
+	
+	static int toPiece(Piece piece) {
+		final int index = fromPieceType(piece.getPieceType());
+		return piece.getPieceSide()==Side.WHITE ? index : -index;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public int getPiece() {
+		return piece;
+	}
+}
