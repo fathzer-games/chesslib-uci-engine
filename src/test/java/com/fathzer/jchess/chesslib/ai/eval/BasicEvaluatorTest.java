4206d521ce2e359f6db9265f496aa11c394189f4
Fathzer <jm-dev@astesana.net>
2024-01-19 13:14:53 +0100

    Adapts to evaluator interfaces simplification

--- src/test/java/com/fathzer/jchess/chesslib/ai/eval/BasicEvaluatorTest.java
+++ src/test/java/com/fathzer/jchess/chesslib/ai/eval/BasicEvaluatorTest.java
@@ -8,7 +8,6 @@ import static com.fathzer.games.MoveGenerator.MoveConfidence.*;
 
 import org.junit.jupiter.api.Test;
 
-import com.fathzer.games.Color;
 import com.fathzer.games.MoveGenerator.MoveConfidence;
 import com.fathzer.games.ai.evaluation.Evaluator;
 import com.fathzer.jchess.chesslib.ChessLibMoveGenerator;
@@ -21,12 +20,11 @@ class BasicEvaluatorTest {
 		private final NaiveEvaluator eval;
 		private final ChessLibMoveGenerator mvg;
 		
-		private ATest(String fen, Color viewPoint, int expectedEval) {
+		private ATest(String fen, int expectedEval) {
 			this.mvg = new ChessLibMoveGenerator(new Board());
 			mvg.getBoard().loadFromFen(fen);
 			this.eval = new NaiveEvaluator();
 			this.eval.init(mvg);
-			eval.setViewPoint(viewPoint);
 			assertEquals(expectedEval, eval.evaluate(mvg));
 		}
 		
@@ -41,58 +39,11 @@ class BasicEvaluatorTest {
 			}
 			assertEquals (expectedEval, incEvaluation, "Error for move "+move+" on "+mvg.getBoard().getFen());
 		}
-		
-		private int unmakeMove() {
-			mvg.unmakeMove();
-			eval.unmakeMove();
-			return eval.evaluate(mvg);
-		}
 	}
 	
 	@Test
-	void testBlack() {
-		ATest test = new ATest("rn1qkb1r/1ppb1ppp/4pn2/pP1p4/3P1B2/4P3/P1P2PPP/RN1QKBNR w KQkq a6 0 6", Color.BLACK, 0);
-		// En passant from white
-		test.test(new Move(B5, A6), -100);
-		// No capture from black
-		test.test(new Move(F8, D6), -100);
-		// Capture from white
-		test.test(new Move(F4, D6), -400);
-		// Capture from black
-		test.test(new Move(C7, D6), -100);
-		// No Capture from white
-		test.test(new Move(B1,B3), -100);
-		// Castling from black
-		test.test(new Move(E8, G8), -100);
-		// Unmake moves
-		assertEquals(-100,test.unmakeMove());
-		assertEquals(-100,test.unmakeMove());
-		assertEquals(-400,test.unmakeMove());
-		assertEquals(-100,test.unmakeMove());
-		assertEquals(-100,test.unmakeMove());
-		assertEquals(0,test.unmakeMove());
-	}
-
-	@Test
-	void testWhite() {
-		ATest test = new ATest("r2qkb1r/1ppb1ppp/4pn2/pP1p4/3P1B2/4P3/P1P2PPP/RN1QKBNR w KQkq a6 0 6", Color.WHITE, 300);
-		// En passant from white
-		test.test(new Move(B5, A6), 400);
-		// No capture from black
-		test.test(new Move(F8, D6), 400);
-		// Capture from white
-		test.test(new Move(F4, D6), 700);
-		// Capture from black
-		test.test(new Move(C7, D6), 400);
-		// No Capture from white
-		test.test(new Move(B1,B3), 400);
-		// Castling from black
-		test.test(new Move(E8, G8), 400);
-	}
-
-	@Test
 	void testCurrentPlayer() {
-		ATest test = new ATest("rn1qkb1r/1ppb1ppp/4pn2/pP1p4/3P1B2/4P3/P1P2PPP/RN1QKBNR w KQkq a6 0 6", null, 0);
+		ATest test = new ATest("rn1qkb1r/1ppb1ppp/4pn2/pP1p4/3P1B2/4P3/P1P2PPP/RN1QKBNR w KQkq a6 0 6", 0);
 		// En passant from white
 		test.test(new Move(B5, A6), -100);
 		// No capture from black
@@ -113,18 +64,17 @@ class BasicEvaluatorTest {
 		mvg.getBoard().loadFromFen("r2qkb1r/1ppb1ppp/4pn2/pP1p4/3P1B2/4P3/P1P2PPP/RN1QKBNR w KQkq a6 0 6");
 		Evaluator<Move, ChessLibMoveGenerator> eval = new NaiveEvaluator();
 		eval.init(mvg);
-		eval.setViewPoint(Color.BLACK);
-		assertEquals(-300, eval.evaluate(mvg));
+		assertEquals(300, eval.evaluate(mvg));
 		
 		ChessLibMoveGenerator mvg2 = (ChessLibMoveGenerator) mvg.fork();
 		Evaluator<Move, ChessLibMoveGenerator> eval2 = eval.fork();
-		assertEquals(-300, eval2.evaluate(mvg2));
+		assertEquals(300, eval2.evaluate(mvg2));
 		// En passant from white
 		Move move = new Move(B5, A6);
-		eval2.prepareMove(mvg, move);
+		eval2.prepareMove(mvg2, move);
 		assertTrue(mvg2.makeMove(move, MoveConfidence.UNSAFE));
 		eval2.commitMove();
 		assertEquals(-400, eval2.evaluate(mvg2));
-		assertEquals(-300, eval.evaluate(mvg));
+		assertEquals(300, eval.evaluate(mvg));
 	}
 }
