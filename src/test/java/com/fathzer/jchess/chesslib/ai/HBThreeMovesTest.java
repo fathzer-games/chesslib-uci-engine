db473f6420718e97c9965e6d313f4cb02f7fee59
Fathzer <jm-dev@astesana.net>
2024-01-25 11:03:22 +0100

    An example of calling engine to have three best moves with their pv

--- src/test/java/com/fathzer/jchess/chesslib/ai/HBThreeMovesTest.java
+++ src/test/java/com/fathzer/jchess/chesslib/ai/HBThreeMovesTest.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/main
 package com.fathzer.jchess.chesslib.ai;
 
 <<<<<<< Upstream, based on origin/main
@@ -35,3 +36,39 @@ class HBThreeMovesTest {
 	}
 
 }
+=======
+package com.fathzer.jchess.chesslib.ai;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.fathzer.games.ai.evaluation.EvaluatedMove;
+import com.fathzer.games.ai.iterativedeepening.IterativeDeepeningEngine;
+import com.fathzer.jchess.chesslib.ChessLibMoveGenerator;
+import com.fathzer.jchess.chesslib.ai.eval.SimplifiedEvaluator;
+import com.fathzer.jchess.chesslib.uci.ChessLibEngine;
+import com.github.bhlangonijr.chesslib.move.Move;
+
+class HBThreeMovesTest {
+
+	@Test
+	void test() {
+		final int depth = 6;
+		final int bestMoveCount = 3;
+		final String fen = "r2k1r2/pp1b2pp/1b2Pn2/2p5/Q1B2Bq1/2P5/P5PP/3R1RK1 w - - 0 1";
+		final IterativeDeepeningEngine<Move, ChessLibMoveGenerator> engine = ChessLibEngine.buildEngine(SimplifiedEvaluator::new, depth);
+		engine.getDeepeningPolicy().setSize(bestMoveCount);
+		final List<EvaluatedMove<Move>> moves = engine.getBestMoves(MinimaxEngineTest.fromFEN(fen, BasicMoveComparator::new));
+		System.out.println(moves);
+		for (int i=0;i<3;i++) {
+			EvaluatedMove<Move> move = moves.get(i);
+			List<Move> principalVariation = move.getPrincipalVariation();
+			System.out.println(move+" -> "+principalVariation);
+		}
+	}
+
+}
+>>>>>>> 949a180 An example of calling engine to have three best moves with their pv
