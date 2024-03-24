da61cae54d26559b5d0d887e226db6f9ababe825
Fathzer <jm-dev@astesana.net>
2024-03-24 19:02:24 +0100

    Improves deepening policy

--- src/main/java/com/fathzer/jchess/chesslib/ai/ChessLibDeepeningPolicy.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/ChessLibDeepeningPolicy.java
@@ -1,9 +1,5 @@
 package com.fathzer.jchess.chesslib.ai;
 
-import java.util.List;
-
-import com.fathzer.games.ai.SearchResult;
-import com.fathzer.games.ai.evaluation.EvaluatedMove;
 import com.fathzer.games.ai.iterativedeepening.DeepeningPolicy;
 
 public final class ChessLibDeepeningPolicy extends DeepeningPolicy {
@@ -21,14 +17,5 @@ public final class ChessLibDeepeningPolicy extends DeepeningPolicy {
 	@Override
 	public boolean isEnoughTimeToDeepen(int depth) {
 		return depth<5 || getSpent()<getMaxTime()/3;
-	}
-
-	@Override
-	public <M> void mergeInterrupted(SearchResult<M> bestMoves, int bestMovesDepth, List<EvaluatedMove<M>> partialList, int interruptionDepth) {
-		if ((interruptionDepth - bestMovesDepth)%2==0) {
-			//TODO Remove when quiesce will be implemented
-			// Do not merge results if depth are optimistic and pessimistic. 
-			super.mergeInterrupted(bestMoves, bestMovesDepth, partialList, interruptionDepth);
-		}
 	}
 }
