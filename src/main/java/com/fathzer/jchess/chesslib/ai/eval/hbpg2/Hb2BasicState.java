7fec468fb77476a9ca8a5457daff8f1f694c13e6
herve <herve@ararat.home>
2024-03-30 07:19:09 +0100

    Valeurs différentes pour les positions des pions en Mg et Eg. L'incrémental concerne désormais seulement le matériel

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2BasicState.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2BasicState.java
@@ -3,6 +3,7 @@ package com.fathzer.jchess.chesslib.ai.eval.hbpg2;
 import static com.fathzer.chess.utils.Pieces.KING;
 
 import com.fathzer.chess.utils.adapters.BoardExplorer;
+import com.github.bhlangonijr.chesslib.Board;
 
 /** The state of the evaluator.
  */
@@ -11,6 +12,7 @@ class Hb2BasicState extends Hb2FastPhaseDetector {
 	int whiteKingIndex;
 	int blackKingIndex;
 	int computedPhase;
+	Board board;
 	
 	
 	Hb2BasicState() {
@@ -23,10 +25,12 @@ class Hb2BasicState extends Hb2FastPhaseDetector {
 		other.blackKingIndex = blackKingIndex;
 		other.whiteKingIndex = whiteKingIndex;
 		other.computedPhase = computedPhase;
+		other.board = board;
+		
 	}
 	
-	Hb2BasicState(BoardExplorer explorer) {
-		
+	Hb2BasicState(BoardExplorer explorer, Board board) {
+		this.board = board;
 		this.points = 0;
 		this.computedPhase = 0;
 		do {
@@ -37,7 +41,7 @@ class Hb2BasicState extends Hb2FastPhaseDetector {
 			final boolean isBlack = p<0;
 			if (kind!=KING) {
 				int inc = Hb2SimplifiedEvaluatorBase.getRawValue(kind);
-				inc += Hb2SimplifiedEvaluatorBase.getPositionValue(kind, isBlack, index);
+//				inc += Hb2SimplifiedEvaluatorBase.getPositionValue(kind, isBlack, index);
 				if (isBlack) {
 					points -= inc;
 				} else {
@@ -57,9 +61,12 @@ class Hb2BasicState extends Hb2FastPhaseDetector {
 	int evaluateAsWhite() {
 		// for the time being (29/03/2024) points are the same in Mg and Eg except for the king preferred squares
 		// but it's gonna change very soon
+		// points = material only, from now on.
 		int phase = getPhaseForTaperedEval(computedPhase);
-		int evalMg = points + Hb2SimplifiedEvaluatorBase.getKingPositionsValueMg(whiteKingIndex, blackKingIndex);
-		int evalEg = points + Hb2SimplifiedEvaluatorBase.getKingPositionsValueEg(whiteKingIndex, blackKingIndex);
+		int pointsPosMg = Hb2SimplifiedEvaluatorBase.getPositionValueMg(board);
+		int pointsPosEg = Hb2SimplifiedEvaluatorBase.getPositionValueEg(board);
+		int evalMg = points + pointsPosMg + Hb2SimplifiedEvaluatorBase.getKingPositionsValueMg(whiteKingIndex, blackKingIndex);
+		int evalEg = points + pointsPosEg+ Hb2SimplifiedEvaluatorBase.getKingPositionsValueEg(whiteKingIndex, blackKingIndex);
 		return ((evalMg * phase + evalEg * (Hb2Phase.NB_INCR_PHASE-phase)) / Hb2Phase.NB_INCR_PHASE);
 	}
 }
