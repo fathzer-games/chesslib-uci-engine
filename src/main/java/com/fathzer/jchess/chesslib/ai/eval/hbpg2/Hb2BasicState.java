b73e44ac0821706a4d23a45aadc915fe1331b927
herve <herve@ararat.home>
2024-10-07 04:58:39 +0200

    Evaluation de la structure de pions: calcul du nombre de pions noirs par colonne, du nombre de pions blancs par colonne. Ca servira pour les pions doublés, les pions passés, etc...

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2BasicState.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2BasicState.java
@@ -3,19 +3,22 @@ package com.fathzer.jchess.chesslib.ai.eval.hbpg2;
 
 
 import com.fathzer.chess.utils.adapters.BoardExplorer;
+import com.fathzer.jchess.chesslib.ai.eval.hbpg2.additional.ChessEvalAdditionalElems;
 import com.github.bhlangonijr.chesslib.Board;
 
 /** The state of the evaluator.
  */
 class Hb2BasicState extends Hb2ElementaryBasicState {
-	
+	private ChessEvalAdditionalElems chessEvalAdditionalElems;
 	
 	Hb2BasicState() {
 		super();
 	}
 	
-	void copyTo(Hb2BasicState other) {
+	public void copyTo(Hb2BasicState other) {
 		super.copyTo(other);
+		chessEvalAdditionalElems.copyTo(other.chessEvalAdditionalElems);
+		
 //		other.pointsMg = pointsMg;
 //		other.pointsEg= pointsEg;
 //		other.pointsPosMg = pointsPosMg;
@@ -31,7 +34,17 @@ class Hb2BasicState extends Hb2ElementaryBasicState {
 	
 	Hb2BasicState(BoardExplorer explorer, Board board) {
 		super(explorer, board);
+		chessEvalAdditionalElems = new ChessEvalAdditionalElems(explorer, board);
+		
 	
+	}
+
+	public ChessEvalAdditionalElems getChessEvalAdditionalElems() {
+		return chessEvalAdditionalElems;
+	}
+
+	public void setChessEvalAdditionalElems(ChessEvalAdditionalElems chessEvalAdditionalElems) {
+		this.chessEvalAdditionalElems = chessEvalAdditionalElems;
 	}
 
 
---
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/additional/ChessEvalAdditionalElems.java
@@ -0,0 +1,34 @@
+package com.fathzer.jchess.chesslib.ai.eval.hbpg2.additional;
+
+import com.fathzer.chess.utils.adapters.BoardExplorer;
+import com.github.bhlangonijr.chesslib.Board;
+
+
+/** This class contains all the chess evaluation stuff that is not in the elementary state of the evaluator.
+ */
+ 
+public class ChessEvalAdditionalElems {
+	
+	private PawnsStrucEval pawnsStructEval;
+	
+	public ChessEvalAdditionalElems(BoardExplorer explorer, Board board) {
+		pawnsStructEval = new PawnsStrucEval(explorer, board);
+	}
+	
+	public void copyTo(ChessEvalAdditionalElems other) {
+		pawnsStructEval.copyTo(other.pawnsStructEval);
+		
+	}
+
+	public PawnsStrucEval getPawnsStructEval() {
+		return pawnsStructEval;
+	}
+
+	public void setPawnsStructEval(PawnsStrucEval pawnsStructEval) {
+		this.pawnsStructEval = pawnsStructEval;
+	}
+	
+	
+	
+
+}
