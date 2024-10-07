9055f78f0bd780cd63e4a567c27957335e0ff0d5
herve <herve@ararat.home>
2024-10-07 03:45:06 +0200

    nettoyage, refactoring

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2BasicState.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2BasicState.java
@@ -1,6 +1,6 @@
 package com.fathzer.jchess.chesslib.ai.eval.hbpg2;
 
-import static com.fathzer.chess.utils.Pieces.KING;
+
 
 import com.fathzer.chess.utils.adapters.BoardExplorer;
 <<<<<<< Upstream, based on origin/main
@@ -11,6 +11,7 @@ import com.github.bhlangonijr.chesslib.Board;
 
 /** The state of the evaluator.
  */
+<<<<<<< Upstream, based on origin/main
 class Hb2BasicState extends Hb2FastPhaseDetector {
 	int pointsMg;
 	int pointsEg;
@@ -22,6 +23,9 @@ class Hb2BasicState extends Hb2FastPhaseDetector {
 	int blackKingIndex;
 	int computedPhase;
 	Board board;
+=======
+class Hb2BasicState extends Hb2ElementaryBasicState {
+>>>>>>> f67222a nettoyage, refactoring
 	
 	
 	Hb2BasicState() {
@@ -30,68 +34,27 @@ class Hb2BasicState extends Hb2FastPhaseDetector {
 	
 	void copyTo(Hb2BasicState other) {
 		super.copyTo(other);
-		other.pointsMg = pointsMg;
-		other.pointsEg= pointsEg;
-		other.pointsPosMg = pointsPosMg;
-		other.pointsPosEg= pointsPosEg;
-		other.blackKingIndex = blackKingIndex;
-		other.whiteKingIndex = whiteKingIndex;
-		other.computedPhase = computedPhase;
-		other.board = board;
+//		other.pointsMg = pointsMg;
+//		other.pointsEg= pointsEg;
+//		other.pointsPosMg = pointsPosMg;
+//		other.pointsPosEg= pointsPosEg;
+//		other.blackKingIndex = blackKingIndex;
+//		other.whiteKingIndex = whiteKingIndex;
+//		other.computedPhase = computedPhase;
+//		other.board = board;
 		
 	}
 	
 
 	
 	Hb2BasicState(BoardExplorer explorer, Board board) {
-		this.board = board;
-		this.pointsMg = 0;
-		this.pointsEg = 0;
-		this.pointsPosMg = 0;
-		this.pointsPosEg = 0;
-		this.computedPhase = 0;
-		do {
-			final int p = explorer.getPiece();
-//			add(p);
-			final int kind = Math.abs(p);
-			final int index = explorer.getIndex();
-			final boolean isBlack = p<0;
-			if (kind!=KING) {
-				int incMg = Hb2SimplifiedEvaluatorBase.getRawValueMg(kind);
-				int incEg = Hb2SimplifiedEvaluatorBase.getRawValueEg(kind);
-				if (isBlack) {
-					pointsMg -= incMg;
-					pointsEg -= incEg;
-				} else {
-					pointsMg += incMg;
-					pointsEg += incEg;
-				}
-			} else if (isBlack) {
-				this.blackKingIndex = index;
-			} else {
-				this.whiteKingIndex = index;
-			}
-			
-			
-			
-			if (kind!=KING) {
-				int incPosMg = Hb2SimplifiedEvaluatorBase.getPositionValueMg(kind, isBlack, index);
-				int incPosEg = Hb2SimplifiedEvaluatorBase.getPositionValueEg(kind, isBlack, index);
-				if (isBlack) {
-					pointsPosMg -= incPosMg;
-					pointsPosEg -= incPosEg;
-				} else {
-					pointsPosMg += incPosMg;
-					pointsPosEg += incPosEg;
-				}
-			}
-			computedPhase += Hb2Phase.getPhaseValue(kind);
-		} while (explorer.next());
+		super(explorer, board);
+	
 	}
 
 
-	int evaluateAsWhite() {
 
+<<<<<<< Upstream, based on origin/main
 		// pointsMg = material only! The white material minus the black material in the middlegame.
 		// pointsEg = material only! The white material minus the black material in the endgame.
 		int phase = getPhaseForTaperedEval(computedPhase);
@@ -286,4 +249,6 @@ class Hb2BasicState extends Hb2FastPhaseDetector {
 >>>>>>> 8fcce8e Valeurs différentes du matériel en final rendues possibles
 		return ((evalMg * phase + evalEg * (Hb2Phase.NB_INCR_PHASE-phase)) / Hb2Phase.NB_INCR_PHASE);
 	}
+=======
+>>>>>>> f67222a nettoyage, refactoring
 }
---
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2ElementaryBasicState.java
@@ -0,0 +1,110 @@
+package com.fathzer.jchess.chesslib.ai.eval.hbpg2;
+
+import static com.fathzer.chess.utils.Pieces.KING;
+
+import com.fathzer.chess.utils.adapters.BoardExplorer;
+import com.github.bhlangonijr.chesslib.Board;
+
+/** The elementary state of the evaluator.
+ */
+ 
+class Hb2ElementaryBasicState {
+	
+	int pointsMg;
+	int pointsEg;
+	int pointsPosMg;
+	int pointsPosEg;
+	int whiteKingIndex;
+	int blackKingIndex;
+	int computedPhase;
+	Board board;
+	
+	
+	Hb2ElementaryBasicState() {
+		super();
+	}
+	
+	void copyTo(Hb2BasicState other) {
+		
+		other.pointsMg = pointsMg;
+		other.pointsEg= pointsEg;
+		other.pointsPosMg = pointsPosMg;
+		other.pointsPosEg= pointsPosEg;
+		other.blackKingIndex = blackKingIndex;
+		other.whiteKingIndex = whiteKingIndex;
+		other.computedPhase = computedPhase;
+		other.board = board;
+		
+	}
+	
+	Hb2ElementaryBasicState(BoardExplorer explorer, Board board) {
+		this.board = board;
+		this.pointsMg = 0;
+		this.pointsEg = 0;
+		this.pointsPosMg = 0;
+		this.pointsPosEg = 0;
+		this.computedPhase = 0;
+		do {
+			final int p = explorer.getPiece();
+
+			final int kind = Math.abs(p);
+			final int index = explorer.getIndex();
+			final boolean isBlack = p<0;
+			if (kind!=KING) {
+				int incMg = Hb2SimplifiedEvaluatorBase.getRawValueMg(kind);
+				int incEg = Hb2SimplifiedEvaluatorBase.getRawValueEg(kind);
+				if (isBlack) {
+					pointsMg -= incMg;
+					pointsEg -= incEg;
+				} else {
+					pointsMg += incMg;
+					pointsEg += incEg;
+				}
+			} else if (isBlack) {
+				this.blackKingIndex = index;
+			} else {
+				this.whiteKingIndex = index;
+			}
+			
+			
+			
+			if (kind!=KING) {
+				int incPosMg = Hb2SimplifiedEvaluatorBase.getPositionValueMg(kind, isBlack, index);
+				int incPosEg = Hb2SimplifiedEvaluatorBase.getPositionValueEg(kind, isBlack, index);
+				if (isBlack) {
+					pointsPosMg -= incPosMg;
+					pointsPosEg -= incPosEg;
+				} else {
+					pointsPosMg += incPosMg;
+					pointsPosEg += incPosEg;
+				}
+			}
+			computedPhase += Hb2Phase.getPhaseValue(kind);
+		} while (explorer.next());
+	}
+
+
+
+	
+	int getPhaseForTaperedEval(int computedPhaseValue) {	
+		return (Math.min(computedPhaseValue, Hb2Phase.PHASE_UPPER_BOUND));
+	}
+	
+	
+	int evaluateAsWhite() {
+
+		// pointsMg = material only! The white material minus the black material in the middlegame.
+		// pointsEg = material only! The white material minus the black material in the endgame.
+//		int phase = Hb2Phase.getPhaseForTaperedEval(computedPhase);
+		// gets the borned phase: necessary for the tapered evaluation
+		int phase= (computedPhase > Hb2Phase.PHASE_UPPER_BOUND?Hb2Phase.PHASE_UPPER_BOUND:computedPhase);
+//		int pointsPosMg = Hb2SimplifiedEvaluatorBase.getPositionValueMg(board);
+//		int pointsPosEg = Hb2SimplifiedEvaluatorBase.getPositionValueEg(board);
+		int evalMg = pointsMg + pointsPosMg + Hb2SimplifiedEvaluatorBase.getKingPositionsValueMg(whiteKingIndex, blackKingIndex);
+		int evalEg = pointsEg + pointsPosEg+ Hb2SimplifiedEvaluatorBase.getKingPositionsValueEg(whiteKingIndex, blackKingIndex);
+		return ((evalMg * phase + evalEg * (Hb2Phase.NB_INCR_PHASE-phase)) / Hb2Phase.NB_INCR_PHASE);
+	}
+
+
+
+}
