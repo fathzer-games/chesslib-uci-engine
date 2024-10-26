82cc1c1329df332377a6fb9e42ca9304238742e7
herve <herve@ararat.home>
2024-10-26 08:03:49 +0200

    multiple pawns of the same color on a column taken into account for pawns structure's evaluation

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2ChessConstants.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2ChessConstants.java
@@ -4,7 +4,9 @@ public abstract class Hb2ChessConstants {
 	public static final int NB_RAWS = 8;
 	public static final int NB_COLS = 8;
 	
-	public static final int MALUS_DOUBLED_PAWN_MG = -20;
-	public static final int MALUS_DOUBLED_PAWN_EG = -40;
+	public static final int MALUS_DOUBLED_PAWNS_MG = -20;
+	public static final int MALUS_DOUBLED_PAWNS_EG = -40;
+	
+	public static final int NB_MAX_PAWNS_TAKEN_INTO_ACCOUNT_FOR_MALUS_DOUBLED_PAWNS = 2; // Beyond, it could be counterproductive...according to many famous chess engines specs
 
 }
