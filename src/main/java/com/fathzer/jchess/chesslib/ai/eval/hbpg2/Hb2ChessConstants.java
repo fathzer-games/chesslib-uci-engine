f3f35a67fa673642b620596c3041a4f60fdb182c
herve <herve@ararat.home>
2024-11-14 12:25:25 +0100

    passed pawns in evaluation pawns' structure: step one

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2ChessConstants.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2ChessConstants.java
@@ -5,8 +5,7 @@ public abstract class Hb2ChessConstants {
 	public static final int NB_FILES = 8;
 	public static final int INDEX_MAX_RANK = NB_RANKS-1;
 	
-	public static final int MALUS_DOUBLED_PAWNS_MG = -20;
-	public static final int MALUS_DOUBLED_PAWNS_EG = -40;
+	
 	
 	public static final int NB_MAX_PAWNS_TAKEN_INTO_ACCOUNT_FOR_MALUS_DOUBLED_PAWNS = 2; // Beyond, it could be counterproductive...according to many famous chess engines specs
 
--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/additional/PawnsStrucEval.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/additional/PawnsStrucEval.java
@@ -7,20 +7,57 @@ import java.util.Arrays;
 import com.fathzer.chess.utils.adapters.BoardExplorer;
 import com.fathzer.chess.utils.adapters.MoveData;
 import com.fathzer.jchess.chesslib.ai.eval.hbpg2.Hb2ChessConstants;
-
+import com.github.bhlangonijr.chesslib.Bitboard;
 import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.File;
+import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.Rank;
+import com.github.bhlangonijr.chesslib.Side;
 
 
 public class PawnsStrucEval {
+	
+	
+	private static final int MALUS_DOUBLED_PAWNS_MG = -20;
+	private static final int MALUS_DOUBLED_PAWNS_EG = -40;
+	
+	private static final int PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK = 13;
+	private static final int PASSED_PAWN_BONUS = 25;
+	private static final int PASSED_PAWN_BONUS_6_TH_RANK = 27;
+	private static final int PASSED_PAWN_BONUS_7_TH_RANK = 30;
+	
+	private static final int PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK_EG = 14;
+	private static final int PASSED_PAWN_BONUS_EG = 27;
+	private static final int PASSED_PAWN_BONUS_6_TH_RANK_EG = 30;
+	private static final int PASSED_PAWN_BONUS_7_TH_RANK_EG = 33;
+	
+	private static final int[] TAB_WHITE_PASSED_PAWN_BONUS_BY_RANK_MG =
+		{0,PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK,
+				PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK,
+				PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK,
+				PASSED_PAWN_BONUS,PASSED_PAWN_BONUS_6_TH_RANK,PASSED_PAWN_BONUS_7_TH_RANK,
+				0};
+	
+	private static final int[] TAB_WHITE_PASSED_PAWN_BONUS_BY_RANK_EG =
+		{0,PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK_EG,
+				PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK_EG,
+				PASSED_PAWN_BONUS_LESS_THAN_5TH_RANK_EG,
+				PASSED_PAWN_BONUS,PASSED_PAWN_BONUS_6_TH_RANK_EG,PASSED_PAWN_BONUS_7_TH_RANK_EG,
+				0};
+	
 	private Board board;
 	
 	private int[] tabNbWhitePawnsByCol;
 	private int[] tabNbBlackPawnsByCol;
-	
+	private int bonusWhitePassedPawns;
+	private int bonusBlackPassedPawns;
 	
 	public PawnsStrucEval() {
 		tabNbWhitePawnsByCol = new int[Hb2ChessConstants.NB_FILES];
 		tabNbBlackPawnsByCol = new int[Hb2ChessConstants.NB_FILES];
+		bonusBlackPassedPawns = 0;
+		bonusWhitePassedPawns = 0;
+		
 	}
 	
 	public PawnsStrucEval(PawnsStrucEval pse) {
@@ -34,6 +71,8 @@ public class PawnsStrucEval {
 	public PawnsStrucEval(BoardExplorer explorer, Board board) {
 		this();
 		
+		long bitboardWhitePawns = board.getBitboard(Piece.WHITE_PAWN);
+		long bitboardBlackPawns = board.getBitboard(Piece.BLACK_PAWN);
 		
 		this.board = board;
 		for (int i= 0; i < Hb2ChessConstants.NB_FILES; i++) {
@@ -55,12 +94,121 @@ public class PawnsStrucEval {
 				} else {
 					tabNbWhitePawnsByCol[columnPawn]++;
 				}
-				
+				if (isPieceBlack) {
+					
+				} else {
+					
+				}
 				
 			} 
 		} while (explorer.next());
 	
 
+	}
+	
+	static int getRgSquare (int rank, int file) {
+		return((Hb2ChessConstants.NB_RANKS*rank)+file);
+	}
+	
+	
+	boolean isPawnPassed(int indexPawnJma, boolean isBlack, long bitboardWhitePawns, long bitboardBlackPawns) {
+		
+		
+		long bitboardEnemyPawns = (isBlack?bitboardWhitePawns:bitboardBlackPawns);
+		if (bitboardEnemyPawns == 0L) {
+			return(true);
+		}
+		int row = Hb2ChessConstants.INDEX_MAX_RANK - indexPawnJma/Hb2ChessConstants.NB_RANKS;
+		int col = indexPawnJma%Hb2ChessConstants.NB_FILES;
+		if (!isBlack) {
+			
+			// Bon, on part de la rangée devant le pion blanc et on va jusqu'à la 7ème rangée, pour la colonne du pion et ses colonnes adjacentes (au max 2)
+			// On regarde s'il y a un pion noir ennemi dans cette zone. Si c'est le cas, alors le pion n'est pas passé. Si cette zone est vide de pions noirs, alors 
+			// le pion blanc est passé
+			 int rankDebut = row +1;
+			 int rankFin = Rank.RANK_7.ordinal();
+			 int fileDebut = col;
+			 int fileFin = col;
+			 if (col == 0) {
+				 fileFin = File.FILE_B.ordinal(); 
+			 } else if (col == 7) {
+				 fileDebut = File.FILE_G.ordinal(); 
+			 }	else {
+				 fileDebut = col - 1;
+				 fileFin = col +1;
+			 }
+			 //getRgSquare
+//			 Square sqDebut = Square.squareAt((Hb2ChessConstants.NB_RANKS*rankDebut)+fileDebut);
+//			 Square sqFin = Square.squareAt((Hb2ChessConstants.NB_RANKS*rankFin)+fileFin);
+//			 int rgDebutSq = (Hb2ChessConstants.NB_RANKS*rankDebut)+fileDebut;
+//			 int rgFinSq = (Hb2ChessConstants.NB_RANKS*rankFin)+fileFin;
+			 boolean areEnemyPawnInZone = false;
+			 for (int rankk = rankDebut; rankk <= rankFin; rankk++) {
+				 int rgSqDebutRank  = getRgSquare(rankk, fileDebut);
+				 int rgSqFintRank  = getRgSquare(rankk, fileFin);
+				 long pionsAdversesDansZoneForRank = Bitboard.bitsBetween(bitboardEnemyPawns, rgSqDebutRank, rgSqFintRank);
+				 if (pionsAdversesDansZoneForRank != 0L) {
+					 areEnemyPawnInZone = true;
+					 return(false);
+				 }
+				 
+			 }
+//			long ALL_BITS_EQUAL_ONE_BITBOARD = 0xFFFFFFFFFFFFFFFFL;
+//			 long pionsAdversesDansZone = Bitboard.bitsBetween(ALL_BITS_EQUAL_ONE_BITBOARD, sqDebut.ordinal(), sqFin.ordinal());
+//			 long pionsAdversesDansZone = Bitboard.bitsBetween(ALL_BITS_EQUAL_ONE_BITBOARD, rgDebutSq, rgFinSq);
+//			 List<Square> lstKazz = Bitboard.bbToSquareList(pionsAdversesDansZone);
+//			 for (Square kaaaaz : lstKazz) {
+//				 System.out.println(kaaaaz.value());
+//			 }
+			 if (areEnemyPawnInZone == false) {
+				 return(true);
+			 }
+
+			 		
+			
+			
+		} else {
+			
+			// Bon, on part de la rangée devant le pion noir (du point de vue noir) et on va jusqu'à la 2ème rangée, pour la colonne du pion et ses colonnes adjacentes (au max 2)
+			// On regarde s'il y a un pion blanc ennemi dans cette zone. Si c'est le cas, alors le pion n'est pas passé. Si cette zone est vide de pions blancs, alors 
+			// le pion noir est passé
+			 int rankDebut = Rank.RANK_2.ordinal(); 
+			 int rankFin = row-1;
+			 int fileDebut =col;
+			 int fileFin = col;
+			 if (col == 0) {
+				 fileFin = File.FILE_B.ordinal(); 
+			 } else if (col == 7) {
+				 fileDebut = File.FILE_G.ordinal(); 
+			 }	else {
+				 fileDebut = col - 1;
+				 fileFin = col +1;
+			 }
+			 
+//			 Square sqDebut = Square.squareAt((Hb2ChessConstants.NB_RANKS*rankDebut)+fileDebut);
+//			 Square sqFin = Square.squareAt((Hb2ChessConstants.NB_RANKS*rankFin)+fileFin);
+			 
+			 boolean areEnemyPawnInZone = false;
+			 for (int rankk = rankDebut; rankk <= rankFin; rankk++) {
+				 int rgSqDebutRank  = getRgSquare(rankk, fileDebut);
+				 int rgSqFintRank  = getRgSquare(rankk, fileFin);
+				 long pionsAdversesDansZoneForRank = Bitboard.bitsBetween(bitboardEnemyPawns, rgSqDebutRank, rgSqFintRank);
+				 if (pionsAdversesDansZoneForRank != 0L) {
+					 areEnemyPawnInZone = true;
+					 return(false);
+				 }
+				 
+			 }
+	
+			 
+			 if (areEnemyPawnInZone == false) {
+				return(true);
+			 }
+			
+			
+		}
+		
+		return(true);
 	}
 	
 	void copyTo(PawnsStrucEval other) {
@@ -137,7 +285,7 @@ public class PawnsStrucEval {
 		int malusW = 0;
 		for (int i = 0; i < Hb2ChessConstants.NB_FILES; i++) {
 			if (tabNbWhitePawnsByCol[i] > 1) {
-				malusW += tabNbWhitePawnsByCol[i] * Hb2ChessConstants.MALUS_DOUBLED_PAWNS_MG;
+				malusW += tabNbWhitePawnsByCol[i] * MALUS_DOUBLED_PAWNS_MG;
 			}
 		}
 		
@@ -145,7 +293,7 @@ public class PawnsStrucEval {
 		int malusB = 0;
 		for (int i = 0; i < Hb2ChessConstants.NB_FILES; i++) {
 			if (tabNbBlackPawnsByCol[i] > 1) {
-				malusB += tabNbBlackPawnsByCol[i] * Hb2ChessConstants.MALUS_DOUBLED_PAWNS_MG;
+				malusB += tabNbBlackPawnsByCol[i] * MALUS_DOUBLED_PAWNS_MG;
 			}
 		}
 		return(malusW - malusB);
@@ -160,7 +308,7 @@ public class PawnsStrucEval {
 		int malusW = 0;
 		for (int i = 0; i < Hb2ChessConstants.NB_FILES; i++) {
 			if (tabNbWhitePawnsByCol[i] > 1) {
-				malusW += Hb2ChessConstants.NB_MAX_PAWNS_TAKEN_INTO_ACCOUNT_FOR_MALUS_DOUBLED_PAWNS * Hb2ChessConstants.MALUS_DOUBLED_PAWNS_EG;
+				malusW += Hb2ChessConstants.NB_MAX_PAWNS_TAKEN_INTO_ACCOUNT_FOR_MALUS_DOUBLED_PAWNS * MALUS_DOUBLED_PAWNS_EG;
 			}
 		}
 		
@@ -168,7 +316,7 @@ public class PawnsStrucEval {
 		int malusB = 0;
 		for (int i = 0; i < Hb2ChessConstants.NB_FILES; i++) {
 			if (tabNbBlackPawnsByCol[i] > 1) {
-				malusB += Hb2ChessConstants.NB_MAX_PAWNS_TAKEN_INTO_ACCOUNT_FOR_MALUS_DOUBLED_PAWNS * Hb2ChessConstants.MALUS_DOUBLED_PAWNS_EG;
+				malusB += Hb2ChessConstants.NB_MAX_PAWNS_TAKEN_INTO_ACCOUNT_FOR_MALUS_DOUBLED_PAWNS * MALUS_DOUBLED_PAWNS_EG;
 			}
 		}
 		return(malusW - malusB);
