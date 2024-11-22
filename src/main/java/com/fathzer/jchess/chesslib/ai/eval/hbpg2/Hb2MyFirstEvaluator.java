32d72bec1c39725fe8b236c8a82da23a710bbe51
herve <herve@ararat.home>
2024-11-22 09:17:12 +0100

    cleaning, among others

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2MyFirstEvaluator.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hbpg2/Hb2MyFirstEvaluator.java
@@ -3,14 +3,13 @@ package com.fathzer.jchess.chesslib.ai.eval.hbpg2;
 import com.fathzer.games.ai.evaluation.StaticEvaluator;
 import com.fathzer.games.ai.evaluation.ZeroSumEvaluator;
 import com.fathzer.jchess.chesslib.ChessLibMoveGenerator;
-import com.fathzer.jchess.chesslib.ai.eval.NaiveEvaluator;
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.move.Move;
 
 
 public class Hb2MyFirstEvaluator implements StaticEvaluator<Move, ChessLibMoveGenerator>, ZeroSumEvaluator<Move, ChessLibMoveGenerator> {
 	private final Hb2SimplifiedEvaluator ev = new Hb2SimplifiedEvaluator();
-	private final NaiveEvaluator enNaive = new NaiveEvaluator();
+//	private final NaiveEvaluator enNaive = new NaiveEvaluator();
 	@Override
 	public int evaluateAsWhite(ChessLibMoveGenerator board) {
 		// Calculer l'évaluation de base
@@ -30,6 +29,22 @@ public class Hb2MyFirstEvaluator implements StaticEvaluator<Move, ChessLibMoveGe
 		return new Hb2MyFirstEvaluator();
 	}
 
+	public static void evaluatePositionAsWhite(String fen) {
+		// Exemple pour créer un ChessLibMoveGenerator à partir d'un FEN et l'évaluer
+		ChessLibMoveGenerator mvg = new ChessLibMoveGenerator(new Board());
+//		mvg.getBoard().loadFromFen("8/3b3p/p3P1p1/3K4/5P1P/2k5/8/8 b - - 0 56");
+//		mvg.getBoard().loadFromFen("5k2/p1p2pp1/4b1p1/4p3/1P2P3/p2P1B2/1b2NP1P/6K1 w - - 0 1");
+//		System.out.println(new Hb2MyFirstEvaluator().evaluate(mvg));
+//		mvg.getBoard().loadFromFen("8/p1p1kppp/4b3/4p1P1/1P2P2P/p2P1B2/1b2NP2/6K1 w - - 0 1");
+		
+		
+		
+		
+	
+		
+		mvg.getBoard().loadFromFen(fen);
+		System.out.println(new Hb2MyFirstEvaluator().evaluate(mvg));
+	}
 
 	public static void main(String[] args) {
 		// Exemple pour créer un ChessLibMoveGenerator à partir d'un FEN et l'évaluer
