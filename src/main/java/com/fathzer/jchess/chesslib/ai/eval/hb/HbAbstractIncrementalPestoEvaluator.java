7c7a9541f7bbc77adfd6e7f4efe15c857cdc1a54
herve <herve@mac.home>
2026-02-21 10:53:04 +0100

    AB

--- src/main/java/com/fathzer/jchess/chesslib/ai/eval/hb/HbAbstractIncrementalPestoEvaluator.java
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hb/HbAbstractIncrementalPestoEvaluator.java
@@ -1,5 +1,6 @@
 package com.fathzer.jchess.chesslib.ai.eval.hb;
 
+import com.fathzer.chess.utils.adapters.BoardExplorer;
 import com.fathzer.chess.utils.evaluators.AbstractChessEvaluator;
 import com.fathzer.chess.utils.evaluators.AbstractPieceSquareTable;
 import com.fathzer.chess.utils.evaluators.pesto.EndGamePieceSquareTable;
@@ -32,7 +33,24 @@ public abstract class HbAbstractIncrementalPestoEvaluator<M, B extends MoveGener
 	public void init(B board) {
 		super.init(board);
 		// utiliser board, qui en vrai est un ChessLibMoveGenerator
+		initCompl(board);
 	
+	}
+	
+	
+	// mettre dans cette fonction le complément HB
+	// toCommit est le state utilisable
+	protected void initCompl(B board) {
+		
+		// ENLEVER CE CODE
+		BoardExplorer explorer = getExplorer(board);
+		do {
+			final int p = explorer.getPiece();
+			final int kind = Math.abs(p);
+			final int index = explorer.getIndex();
+			final boolean isBlack = p<0;
+			put(kind, isBlack, index);
+		} while (explorer.next());
 	}
 	
     @Override
---
+++ src/main/java/com/fathzer/jchess/chesslib/ai/eval/hb/compl/HbComplPestoEval.java
@@ -0,0 +1,15 @@
+package com.fathzer.jchess.chesslib.ai.eval.hb.compl;
+
+import com.fathzer.jchess.chesslib.ChessLibMoveGenerator;
+
+public class HbComplPestoEval {
+	private ChessLibMoveGenerator chessLibMoveGenerator;
+	private int complEval;
+
+	public HbComplPestoEval(ChessLibMoveGenerator chessLibMoveGenerator) {
+		super();
+		this.chessLibMoveGenerator = chessLibMoveGenerator;
+	}
+	
+
+}
